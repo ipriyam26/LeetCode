23d012efa8e6a67bb3574cd196015d9948fbf824
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-18 20:14:04 +0530

    Scripted auto-commit on change (2022-09-18 20:14:04) by gitwatch.sh

--- 215.kth-largest-element-in-an-array.py
+++ 215.kth-largest-element-in-an-array.py
@@ -5,8 +5,17 @@
 #
 
 # @lc code=start
+from typing import List
+import heapq
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        minHeap:heapq = heapq()
+        for num in nums:
+            minHeap.heappush(num)
+            if len(minHeap)>k:
+                minHeap.heappop()
+        return minHeap.heappushpop()
         
 # @lc code=end
 
