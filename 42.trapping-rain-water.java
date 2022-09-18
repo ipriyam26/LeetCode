c78b50b9249a44eeb6624b24620ffd87c190af95
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-18 19:08:59 +0530

    # modified:   42.trapping-rain-water.java

--- 42.trapping-rain-water.java
+++ 42.trapping-rain-water.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
  * @lc app=leetcode id=42 lang=java
  *
@@ -6,8 +8,46 @@
 
 // @lc code=start
 class Solution {
+
+    public int[] largestToRight(int[] height){
+        int[] result = new  int[height.length];
+        int max=0;
+        for(int i =0;i<height.length;i++){
+            result[i]= max;
+            if(height[i]>max){
+                max = height[i];
+            }
+        }
+        return result;
+    }
+
+    public int[] largestToLeft(int[] height){
+        int[] result = new  int[height.length];
+        int max=0;
+        for(int i =height.length-1;i>=0;i--){
+            result[i]= max;
+            if(height[i]>max){
+                max = height[i];
+            }
+        }
+        return result;
+    }
+
     public int trap(int[] height) {
-        
+        // int[] LFR = largest_from_right(height);
+        int[] LTR = largestToRight(height);
+        int[] LTL = largestToLeft(height);
+        System.out.println(Arrays.toString(LTR));
+        System.out.println(Arrays.toString(LTL));
+        // int[] result = new int[height.length];
+        int result=0;
+        for(int i=0;i<height.length;i++){
+            int value = Math.min(LTR[i], LTL[i]) - height[i];
+           if(value>0){
+            result+=value;
+           }
+        }
+        return result;
     }
 }
 // @lc code=end
