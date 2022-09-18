da78dd96e326fcdee3b5d1989d5c821e929732ae
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-18 20:10:38 +0530

    Scripted auto-commit on change (2022-09-18 20:10:38) by gitwatch.sh

---
+++ 215.kth-largest-element-in-an-array.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=215 lang=python3
+#
+# [215] Kth Largest Element in an Array
+#
+
+# @lc code=start
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        
+# @lc code=end
+
