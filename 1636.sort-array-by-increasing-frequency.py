3622c73aaeab0120b74131a2206038cfa0357f0b
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-20 18:22:28 +0530

    Scripted auto-commit on change (2022-09-20 18:22:28) by gitwatch.sh

--- 1636.sort-array-by-increasing-frequency.py
+++ 1636.sort-array-by-increasing-frequency.py
@@ -5,43 +5,31 @@
 #
 
 # @lc code=start
+from collections import Counter
 from dataclasses import dataclass
 from typing import Dict, List
 import heapq
 
-@dataclass
-class Pair:
-   count:int
-   value:int
-   def __lt__(self,other):
-       if self.count<other.count:
-           return True
-       elif self.count>other.count:
-           return False
-       else:
-           return self.count<other.count 
+
 
 class Solution:
     
-    def make_frequency_map(self,nums: List[int])-> Dict[int,int]:
-        hash_map = {}
-        for num in nums:
-            if num in hash_map:
-                hash_map[num]+=1
-            else:
-                hash_map[num]=1
-        return hash_map
     
     def frequencySort(self, nums: List[int]) -> List[int]:
-       frequency_count = self.make_frequency_map(nums)
-       ans = []
-       for value,count in frequency_count.items():
-           heapq.heappush(ans,Pair(count=count,value=value))
-       result = []
-       while ans:
-          pooped:Pair = heapq.heappop(ans)
-          result.extend(pooped.value for _ in range(pooped.count))
-       return result
+        
+        counts = Counter(nums)
+
+        heap = []
+        for key in counts:
+            heapq.heappush(heap, (counts[key], (-1 *key)))
+
+        output = []
+        while heap:
+            count, val = heapq.heappop(heap)
+            val *= -1
+
+            output.extend(val for _ in range(count))
+        return output	
         
 
         
