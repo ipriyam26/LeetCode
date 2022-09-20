72604ee9c94eb6b0b06c720bc587b8a5ab39dd80
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-20 18:02:37 +0530

    Scripted auto-commit on change (2022-09-20 18:02:37) by gitwatch.sh

---
+++ 1636.sort-array-by-increasing-frequency.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1636 lang=python3
+#
+# [1636] Sort Array by Increasing Frequency
+#
+
+# @lc code=start
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        
+# @lc code=end
+
