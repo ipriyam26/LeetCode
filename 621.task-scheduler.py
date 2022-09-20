f89379f34ab215c0ddb1fde9a1e1534548e11f25
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-20 21:18:22 +0530

    Scripted auto-commit on change (2022-09-20 21:18:22) by gitwatch.sh

--- 621.task-scheduler.py
+++ 621.task-scheduler.py
@@ -24,12 +24,14 @@ class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         freq:Dict[str,int] = self.make_frequency_map(tasks)
         holder = []
+        print(freq)
         for item,count in freq.items():
             heappush(holder,(0,item,count))
         time=0
         while holder:
             time+=1
             pooped = heappop(holder)
+            print(pooped)
             time = max(time, pooped[0])
             if count<=1:
                 continue
