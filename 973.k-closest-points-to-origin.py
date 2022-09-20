47a6588d15bcf8297616aee9d1c3ef9149d9ad17
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-20 18:38:38 +0530

    Scripted auto-commit on change (2022-09-20 18:38:38) by gitwatch.sh

--- 973.k-closest-points-to-origin.py
+++ 973.k-closest-points-to-origin.py
@@ -5,11 +5,25 @@
 #
 
 # @lc code=start
+import heapq
 from typing import List
 
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        
+        distance = [] 
+        for dimensions in points:
+            x,y = dimensions
+            heapq.heappush(distance,(-1*(x*x+y*y)**0.5,dimensions))
+            if len(distance):
+                heapq.heappop(distance)
+        result = []
+        while distance:
+            result.index(0,heapq.heappop(distance)[1])
+        return result
+            
+            
+            
+           
 # @lc code=end
 
