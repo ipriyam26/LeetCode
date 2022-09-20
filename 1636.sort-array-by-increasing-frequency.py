c7a0022180f47b4498628f81cffc236144730a34
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-20 18:05:10 +0530

    Scripted auto-commit on change (2022-09-20 18:05:10) by gitwatch.sh

--- 1636.sort-array-by-increasing-frequency.py
+++ 1636.sort-array-by-increasing-frequency.py
@@ -5,8 +5,28 @@
 #
 
 # @lc code=start
+from dataclasses import dataclass
+from typing import List
+import heapq
+
+@dataclass
+class Pair:
+   count:int
+   value:int
+   def __lt__(self,other):
+       if self.count<other.count:
+           return True
+       elif self.count>other.count:
+           return False
+       else:
+           return self.count>other.count 
+
 class Solution:
+    
+    
     def frequencySort(self, nums: List[int]) -> List[int]:
+        
+
         
 # @lc code=end
 
