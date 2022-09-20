bad05bc7c57c3c6a629c43ac5b32a2904cf35fc5
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-20 18:28:00 +0530

    Scripted auto-commit on change (2022-09-20 18:28:00) by gitwatch.sh

---
+++ 973.k-closest-points-to-origin.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=973 lang=python3
+#
+# [973] K Closest Points to Origin
+#
+
+# @lc code=start
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+# @lc code=end
+
