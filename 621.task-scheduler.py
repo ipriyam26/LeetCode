2fe7aea8450552d65fe37bd146932452f86cb7ae
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-20 21:07:28 +0530

    Scripted auto-commit on change (2022-09-20 21:07:28) by gitwatch.sh

---
+++ 621.task-scheduler.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=621 lang=python3
+#
+# [621] Task Scheduler
+#
+
+# @lc code=start
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        
+# @lc code=end
+
