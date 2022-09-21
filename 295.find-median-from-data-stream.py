342400f97fc1334833d7331acfd295986a61f324
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-21 22:01:25 +0530

    Scripted auto-commit on change (2022-09-21 22:01:25) by gitwatch.sh

--- 295.find-median-from-data-stream.py
+++ 295.find-median-from-data-stream.py
@@ -5,12 +5,33 @@
 #
 
 # @lc code=start
+from typing import List
+
+
 class MedianFinder:
 
     def __init__(self):
-        
+        self.is_odd = None
+        self.values = []
+        self.median_index = 0
+    
+    def _find_right_place_to_insert(self,nums:List[int],num:int)  :
+        high = len(nums)-1
+        low = 0
+        while low<=high:
+            mid = (low+high)//2
+            if(nums[mid]>num):
+                high = mid-1
+            elif(nums[mid]<num):
+                low = mid+1
+            else:
+                return mid
+        return high-1
+    
+                
 
     def addNum(self, num: int) -> None:
+        
         
 
     def findMedian(self) -> float:
