59ab614925671c2f170651f58bd3cdccbdd2aa4a
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-21 19:46:00 +0530

    Scripted auto-commit on change (2022-09-21 19:46:00) by gitwatch.sh

--- 846.hand-of-straights.py
+++ 846.hand-of-straights.py
@@ -43,8 +43,8 @@ class Solution:
                 if pooped[1]>1:
                     buffer.append((pooped[0],pooped[1]-1))
                 prev = pooped[0]
-                print(pooped)
-            print('-'*30)
+                # print(pooped)
+            # print('-'*30)
             
             for item in buffer:
                 heapq.heappush(holder,item)
