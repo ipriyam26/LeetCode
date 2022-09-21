b2723ce613c83a757c655f677932fa321be74c20
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-21 19:18:30 +0530

    Scripted auto-commit on change (2022-09-21 19:18:30) by gitwatch.sh

--- 846.hand-of-straights.py
+++ 846.hand-of-straights.py
@@ -5,8 +5,31 @@
 #
 
 # @lc code=start
+import heapq
+from typing import List
+
+
 class Solution:
+        
+    def make_frequency_map(self,nums: List[int]):
+        hash_map = {}
+        for num in nums:
+            if num in hash_map:
+                hash_map[num]+=1
+            else:
+                hash_map[num]=1
+        return hash_map
+    
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        freq_list = self.make_frequency_map(hand)
+        if(len(hand)%groupSize!=0):
+            return False
+        holder = []
+        
+        for value,count in freq_list.items():
+            heapq.heappush(holder,(value,count))
+        
+        
         
 # @lc code=end
 
