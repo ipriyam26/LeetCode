2f55c4d2af27d09f47af1f552c8f725fb331a081
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-21 21:52:29 +0530

    Scripted auto-commit on change (2022-09-21 21:52:29) by gitwatch.sh

---
+++ 295.find-median-from-data-stream.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=295 lang=python3
+#
+# [295] Find Median from Data Stream
+#
+
+# @lc code=start
+class MedianFinder:
+
+    def __init__(self):
+        
+
+    def addNum(self, num: int) -> None:
+        
+
+    def findMedian(self) -> float:
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+# @lc code=end
+
