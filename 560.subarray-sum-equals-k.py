fb739c4014c6416dacda9c78e47baed7175ddc33
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-23 21:06:06 +0530

    Scripted auto-commit on change (2022-09-23 21:06:06) by gitwatch.sh

--- 560.subarray-sum-equals-k.py
+++ 560.subarray-sum-equals-k.py
@@ -10,7 +10,24 @@ from typing import List
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        
+        i,j=0,0
+        max_length  = 0
+        value = 0
+        while j<len(nums):
+            value+=nums[j]
+            if(value<k):
+                continue
+            elif(value==k):
+                if i-j+1>max_length:
+                    max_length = i-j+1
+            else:
+                while(value>k):
+                    i+=1
+                    value-=nums[i]
+                    
+            j+=1
+                
+            
         
 # @lc code=end
 
