d3ccb3c861616412bc16534035263e5a85225195
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-03 21:49:04 +0530

    This optimiztion didn't make it faster

--- 1004.max-consecutive-ones-iii.py
+++ 1004.max-consecutive-ones-iii.py
@@ -12,25 +12,28 @@ class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         i,j=0,0
         max_count = 0
-        zero_count=[]
+        zero_count=0
+        zero_location = []
         
         while j < len(nums):
             if(nums[j]==0):
-                zero_count.append(j)
+                zero_count+=1
+                zero_location.append(j)
                 
-            if (len(zero_count) <= k) and (max_count < j - i + 1):
+            if (zero_count <= k) and (max_count < j - i + 1):
                 max_count= j-i+1
-            elif(len(zero_count) >k):
-                i = zero_count[0]+1
-                zero_count.pop(0)
+            elif(zero_count >k):
+                i=zero_location[0]+1
+                zero_location.pop(0)
+                zero_count=k
             j+=1
         
         return max_count
 
-if __name__=='__main__':
-    arr = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1]
-    k =3
-    print(Solution().longestOnes(arr,k))
+# if __name__=='__main__':
+#     arr = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1]
+#     k =3
+#     print(Solution().longestOnes(arr,k))
         
 # @lc code=end
 
