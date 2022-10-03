1a34c928bf0b01e4ab06fca35da74aaaec17a436
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-03 20:25:28 +0530

    Scripted auto-commit on change (2022-10-03 20:25:28) by gitwatch.sh

--- 3.longest-substring-without-repeating-characters.py
+++ 3.longest-substring-without-repeating-characters.py
@@ -14,19 +14,24 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         i,j = 0,0
         max_value = 0
-        holder = defaultdict(self.default_fill)
+        holder = {}
         while j <len(s):
-            holder[s[j]]+=1
-            if (j-i+1==len(holder)):
-                if(j-i+1>max_value):
-                    max_value = j-i+1
-            else:
-                while(j-i+1!=len(holder)and i<j):
-                    holder[s[i]]-=1
-                    if holder[s[i]]==0:
-                        holder.pop(s[i])  
-                    i+=1
-            j+=1
+            # holder[s[j]]+=1
+            # if (j-i+1==len(holder)):
+            #     if(j-i+1>max_value):
+            #         max_value = j-i+1
+            # else:
+            #     while(j-i+1!=len(holder)and i<j):
+            #         holder[s[i]]-=1
+            #         if holder[s[i]]==0:
+            #             holder.pop(s[i])  
+            #         i+=1
+            # j+=1
+            if(s[j] in holder and s[j]>i):
+                i = s[j]
+            elif j-i+1 > max_value:
+                max_value=j-i+1
+            s[j]=j
         return max_value
             
             
