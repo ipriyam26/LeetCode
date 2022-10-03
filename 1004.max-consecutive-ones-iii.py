c89615c711aeb98eac1a9b9e6a8fa63c32b62f43
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-03 21:33:21 +0530

    Scripted auto-commit on change (2022-10-03 21:33:21) by gitwatch.sh

---
+++ 1004.max-consecutive-ones-iii.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=1004 lang=python3
+#
+# [1004] Max Consecutive Ones III
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        i,j=0,0
+        max_count = 0
+        zero_count=0
+        while j < len(nums):
+            if(nums[j]==0):
+                zero_count+=1
+                
+            if (zero_count <= 2) and (max_count < j - i + 1):
+                max_count= j-i+1
+            elif(zero_count >2):
+                while zero_count>2 and i<j:
+                    if(nums[i]==0) :
+                        zero_count-=1
+                    i+=1
+            j+=1
+        
+        return max_count
+
+if __name__=='__main__':
+    arr = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1]
+    k =3
+    print(Solution().longestOnes(arr,k))
+        
+# @lc code=end
+
---
+++ 485.max-consecutive-ones.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=485 lang=python3
+#
+# [485] Max Consecutive Ones
+#
+
+# @lc code=start
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        
+# @lc code=end
+
