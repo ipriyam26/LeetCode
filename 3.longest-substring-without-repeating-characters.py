2b7517d4f5bfd0ff53fd2d7d853a2c2c88f4acec
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-03 20:13:26 +0530

    Scripted auto-commit on change (2022-10-03 20:13:26) by gitwatch.sh

--- 3.longest-substring-without-repeating-characters.py
+++ 3.longest-substring-without-repeating-characters.py
@@ -5,21 +5,26 @@
 #
 
 # @lc code=start
+from collections import defaultdict
+from email.policy import default
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         i,j = 0,0
         max_value = 0
-        holder = set()
+        holder = defaultdict(lambda _:0)
         while j <len(s):
             holder.add(s[j])
             if (j-i+1==len(holder)):
                 if(j-i+1>max_value):
                     max_value = j-i+1
             else:
-                while(s[i]!=s[j] and i<j):
+                while(j-i+1!=len(holder)and i<j):
+                    holder[s[i]]-=1
+                    if holder[s[i]]==0:
+                        holder.pop(s[i])  
                     i+=1
-                    holder.remove(s[i])
-                i+=1
             j+=1
         return max_value
             
