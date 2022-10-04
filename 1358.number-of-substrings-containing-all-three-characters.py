c402138a46ebd38d6a4929a8dce1114aeecc3788
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-04 22:42:59 +0530

    Number of substrings containing of all three characters

--- 1358.number-of-substrings-containing-all-three-characters.py
+++ 1358.number-of-substrings-containing-all-three-characters.py
@@ -7,6 +7,20 @@
 # @lc code=start
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        
+        hashStore = {
+            'a':0,
+            'b':0,
+            'c':0
+        }
+        result = 0
+        i=0
+        for j in range(len(s)):
+            hashStore[s[j]]+=1
+            while all(hashStore.values()):
+                hashStore[s[i]]-=1
+                i+=1
+            result+=i
+        return result
+                
 # @lc code=end
 
