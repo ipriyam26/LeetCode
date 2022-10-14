2ee4ff5800d34d9027a09ee252b3e2613bcb2a66
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-14 19:22:30 +0530

    Lcs

--- 1143.longest-common-subsequence.py
+++ 1143.longest-common-subsequence.py
@@ -1,12 +1,27 @@
-#
-# @lc app=leetcode id=1143 lang=python3
-#
-# [1143] Longest Common Subsequence
-#
-
-# @lc code=start
+import itertools
 class Solution:
+    
+    
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m=len(text2)
+        n = len(text1)
+        self.dp = [[ -1 for _ in range(m+1)] for _ in range(n+1)]
+        for i in range (n+1):
+            self.dp[i][0]=0
+
+        for j in range(m+1):
+            self.dp[0][j] =0
+        i,j=0,0
+        for i, j in itertools.product(range(1,n+1), range(1,m+1)):
+            if text1[i-1] == text2[j-1]:
+                self.dp[i][j] = self.dp[i - 1][j - 1] +1 
+            else:
+                self.dp[i][j]=max(self.dp[i - 1][j], self.dp[i][j - 1])
+
+        return self.dp[n][m]
+if __name__ == "__main__":
+    print(Solution().longestCommonSubsequence("abcde","ace"))
+    
         
 # @lc code=end
 
