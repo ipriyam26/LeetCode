f2160e7c50847f361c0e44e421698020ba0d14cb
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-11-06 18:13:26 +0530

    Scripted auto-commit on change (2022-11-06 18:13:26) by gitwatch.sh

--- 72.edit-distance.py
+++ 72.edit-distance.py
@@ -6,7 +6,15 @@
 
 # @lc code=start
 class Solution:
+    def findMinium(self,i:int,j:int,word1:str,word2:str):
+        if i<0:
+            return 1 if j<0 else 1+ self.findMinium(i,j-1,word1=word1,word2=word2)
+        if j<0:
+            return 1 + self.findMinium(i-1,j,word1=word1,word2=word2)
+        
+    
     def minDistance(self, word1: str, word2: str) -> int:
+
         
 # @lc code=end
 
