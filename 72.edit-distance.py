cd46e3bbed602c886bd227e3cf7fbb02f4b5eb57
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-11-06 16:13:56 +0530

    Scripted auto-commit on change (2022-11-06 16:13:55) by gitwatch.sh

---
+++ 72.edit-distance.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=72 lang=python3
+#
+# [72] Edit Distance
+#
+
+# @lc code=start
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        
+# @lc code=end
+
