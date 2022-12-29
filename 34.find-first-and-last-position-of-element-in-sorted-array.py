7905c85ac9d96a5e774d0331b0e77b9e44486fad
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-12-29 14:13:17 +0530

    problem 3

--- 34.find-first-and-last-position-of-element-in-sorted-array.py
+++ 34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -9,10 +9,12 @@ from typing import List
 
 
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
+    
+    
+    
+    def find_position_to_insert(self, nums: List[int], target: int) -> int:
         start = 0
         end = len(nums)-1
-        s,e = -1,-1
         while start<=end:
             mid = (start+end)//2
             if nums[mid]<target:
@@ -20,16 +22,40 @@ class Solution:
             elif nums[mid]>target:
                 end= mid-1
             else:
-                # this is the first occurence
-                if nums[mid-1]!=target:
-                    s=mid
-                    start=mid+1
-                if nums[mid+1]!=target:
-                    e=mid
-                    end+=mid-1
-                if s !=-1 and e!=-1:
-                    return [s,e]
-        return [s,e]
+                return mid
+        return end + 1
+    
+    def search_exist(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums)-1
+        while start<=end:
+            mid = (start+end)//2
+            if nums[mid]<target:
+                start=mid+1
+            elif nums[mid]>target:
+                end= mid-1
+            else:
+                return mid
+        return -1
+    
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if self.search_exist(nums,target) ==-1:
+            return [-1,-1]
+        
+        # now that we are sure that the number exists
+        
+        # we can the correct place to insert target - 0.1 ( i.e we find try to index of the first occurrence of the letter)
+        
+        # any number slightly smaller than target would be inserted at the first occurence of target
+        
+        start = self.find_position_to_insert(nums=nums,target=target-0.1)
+        
+        # we do just the opposite and try to find a number that slightly larger than the target and get correct place to insert it
+        
+        # now remember that the place to insert a number slightly greater that target will be last occurence of target + 1 (i.e just after the target) so we subtract 1 to get last occurence of target   
+        end = self.find_position_to_insert(nums=nums,target=target+0.1) - 1
+        return [start,end]
+
         
 # @lc code=end
 
