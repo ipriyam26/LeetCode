30f62c49f72ec548a1611ca61ca5c1a3d221c00d
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-02-06 12:44:54 +0530

    Scripted auto-commit on change (2023-02-06 12:44:54) by gitwatch.sh

---
+++ 371.sum-of-two-integers.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=371 lang=java
+ *
+ * [371] Sum of Two Integers
+ */
+
+// @lc code=start
+class Solution {
+    public int getSum(int a, int b) {
+        
+    }
+}
+// @lc code=end
+
