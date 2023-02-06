ad4edc8eca5dc2911b6983f08659fa386ee8b24d
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-02-06 14:48:43 +0530

    Scripted auto-commit on change (2023-02-06 14:48:43) by gitwatch.sh

--- 191.number-of-1-bits.java
+++ 191.number-of-1-bits.java
@@ -9,7 +9,7 @@ public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
        int count =0;
-       while(n>=0){
+       while(n>0){
        n&= (n-1);
        count+=1;
        }
