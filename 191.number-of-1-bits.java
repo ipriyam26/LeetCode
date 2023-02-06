b9fd777ca015a8633907e52a18cbc74d7e8cd20e
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-02-06 14:47:49 +0530

    Scripted auto-commit on change (2023-02-06 14:47:49) by gitwatch.sh

--- 191.number-of-1-bits.java
+++ 191.number-of-1-bits.java
@@ -10,8 +10,7 @@ public class Solution {
     public int hammingWeight(int n) {
        int count =0;
        while(n>=0){
-        count+=n&1;
-        n>>=1;
+       
        }
        return count; 
     }
