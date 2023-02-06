0fbb0b3c13409e17a27551f175709a9debac6c2b
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-02-06 12:18:20 +0530

    Scripted auto-commit on change (2023-02-06 12:18:20) by gitwatch.sh

--- 53.maximum-subarray.java
+++ 53.maximum-subarray.java
@@ -7,7 +7,20 @@
 // @lc code=start
 class Solution {
     public int maxSubArray(int[] nums) {
+
+       int   sum=nums[0]; 
+       int maxSum = sum;
+       int p;
+       for (int i = 1; i < nums.length; i++) {
+           
         
+        p= nums[i]+sum;
+        sum = p>nums[i]?p:nums[i];
+        maxSum = Math.max(maxSum, sum);
+
+       }
+       return maxSum;
+
     }
 }
 // @lc code=end
