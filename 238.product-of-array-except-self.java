c5901f6088fe8159d6848365dc62897f54eb7993
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-02-06 11:53:54 +0530

    Scripted auto-commit on change (2023-02-06 11:53:54) by gitwatch.sh

--- 238.product-of-array-except-self.java
+++ 238.product-of-array-except-self.java
@@ -9,16 +9,20 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
        int product = 1;
        int productIgnoringZeros = 1;
+       int zeroCount = 0;
        for (int i : nums) {
         product*=i;
         if(i!=0){
             productIgnoringZeros*=i;
         }
+        else{
+            zeroCount++;
+        }
        } 
        int[] result = new int[nums.length];
 
        for (int i = 0; i < result.length; i++) {
-        if(nums[i]==0){
+        if(nums[i]==0 && zeroCount==1){
             result[i] = productIgnoringZeros;
         }
         else{
