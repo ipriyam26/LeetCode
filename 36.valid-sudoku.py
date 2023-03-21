22e8afc3515b1f3b45a8f0dc9166dbc6c344440f
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-03-21 20:14:14 +0530

    valid suduko

--- 36.valid-sudoku.py
+++ 36.valid-sudoku.py
@@ -5,11 +5,28 @@
 #
 
 # @lc code=start
+import itertools
 from typing import List
 
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        columns = [set() for _ in range(9)]
+        squares = [[set() for _ in range(3)] for _ in range(3)]
+
+        for x, y in itertools.product(range(9), range(9)):
+            cell_value = board[x][y]
+            if cell_value == ".":
+                continue
+            if cell_value in rows[x] or cell_value in columns[y] or cell_value in squares[x//3][y//3]:
+                return False
+
+            rows[x].add(cell_value)
+            columns[y].add(cell_value)
+            squares[x//3][y//3].add(cell_value)
+
+        return True
         
 # @lc code=end
 
